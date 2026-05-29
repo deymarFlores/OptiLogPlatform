e86acdbfd612d12db1e6f99a7867a78da052bc22
deymarFlores <deymarfl127@gmail.com>
2026-05-28 20:44:32 -0400

    Feature: Se agregan los routers al router centralizador

--- Backend/src/modules/router.py
+++ Backend/src/modules/router.py
@@ -1,7 +1,21 @@
 from fastapi import APIRouter
+from src.modules.auth.router.Login import router as login_router
+from src.modules.auth.router.Register import router as register_router
+from src.modules.typeLocation.router.TypeLocationRouter import router as type_location_router
+from src.modules.typeMaterials.router.TypeMaterialRouter import router as type_material_router
+from src.modules.materials.router.MaterialRouter import router as material_router
+from src.modules.materials.router.LocationStockRouter import router as location_stock_router
+from src.modules.companies.router.Company import router as company_router
+from src.modules.locationsCompany.router.LocationCompany import router as location_company_router
+
 
 router = APIRouter()
 
-@router.get("/")
-def root():
-    return {"message": "router funcionando"}
+router.include_router(login_router)
+router.include_router(register_router)
+router.include_router(company_router)
+router.include_router(location_company_router)
+router.include_router(type_location_router)
+router.include_router(type_material_router)
+router.include_router(material_router)
+router.include_router(location_stock_router)
