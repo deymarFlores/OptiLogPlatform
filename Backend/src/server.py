13675801d014390f8c7f9a690b30401b952942be
deymarFlores <deymarfl127@gmail.com>
2026-05-30 09:42:14 -0400

    Feature: Se agrega la configuracion y manejo de CORS

--- Backend/src/server.py
+++ Backend/src/server.py
@@ -1,14 +1,12 @@
 from fastapi import FastAPI
 from src.modules.router import router as AppRouter
+from fastapi.middleware.cors import CORSMiddleware
 
 
 class Server:
     def __init__(self, port: int = 8000):
         self.port = port
-        self.app = FastAPI(
-            title="Mi API",
-            version="1.0.0"
-        )
+        self.app = FastAPI(title="Mi API", version="1.0.0")
         self._add_startup_event()
 
     def _add_startup_event(self):
@@ -18,8 +16,21 @@ class Server:
             print(f"Docs: http://127.0.0.1:{self.port}/docs")
 
     def setup_middlewares(self):
-        # CORS
-        pass
+        self.app.add_middleware(
+            CORSMiddleware,
+            allow_origins=[
+                "http://localhost:5173",
+                "http://127.0.0.1:5173",
+                "http://localhost:3000",
+                "http://localhost:8080",
+                "http://127.0.0.1:8000",
+            ],
+            allow_credentials=True,
+            allow_methods=["*"],
+            allow_headers=["*"],
+            expose_headers=["*"],
+            max_age=3600,
+        )
 
     def setup_routes(self):
         self.app.include_router(AppRouter)
