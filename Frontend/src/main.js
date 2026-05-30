304b4b2a35a704cc94a391d782e5c9d760323c34
deymarFlores <deymarfl127@gmail.com>
2026-05-30 18:41:19 -0400

    Se implementa configuraciones y archivos de variables de entorno para el funcionamiento

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue'
 import { router } from '@/router'
 import '@fortawesome/fontawesome-free/css/all.css'
 import 'leaflet/dist/leaflet.css'
+import './Styles.css'
 
 createApp(App)
   .use(router)
